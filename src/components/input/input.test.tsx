881a66bf2ee3f8bdabf5e17322ef740a55729eb5
juanjoseortizgamez <juanjoseortizgamez@gmail.com>
2024-07-28 15:42:01 +0000

    creating test plan for input

---
+++ src/components/input/input.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+
+describe('Input', () => {
+    test('should render the component with the placeholder', () => {})
+    test('should apply the correct class if isCorrect', () => {})
+    test('should apply the wrong class if isWrong', () => {})
+    test("should be disabled if isn't is active", () => {})
+    test('should be available if is active', () => {})
+    test('should have value if the value is passed', () => {})
+    test('should onChange event works', () => {})  
+})
